dfc84c8e477324079938974abc18d49b67834944
nwong02 <nelsonwong107@gmail.com>
2022-02-21 18:23:07 -0500

    Add project files.

---
+++ Samples.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+* Title			: Samples.cpp
+* Project		: Arrays (for learning only)
+* Author		: Nelson Wong
+* Description	: Pulled sample lessons from codecademy
+* Date			: Feb. 21, 2022
+*/
+
+int main(void)
+{
+	double q[5];		// uninitialised array
+	int evens[5] = { 2, 4, 6, 8, 10 }; // initialised array
+
+	return 0;
+}
