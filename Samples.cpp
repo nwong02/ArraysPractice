3cc927c1fdfc874f994f7e25da8dd4aa308c4bd4
nwong02 <nelsonwong107@gmail.com>
2022-02-21 18:29:05 -0500

    Array reassignment added

--- Samples.cpp
+++ Samples.cpp
@@ -10,8 +10,18 @@
 
 int main(void)
 {
-	double q[5];		// uninitialised array
-	int evens[5] = { 2, 4, 6, 8, 10 }; // initialised array
+	//double q[5];		// uninitialised array
+	//int evens[5] = { 2, 4, 6, 8, 10 }; // initialised array
+
+	
+	
+	int evens[] = { 2, 4, 6, 8, 10, 12 };
+	int odds[] = { 1, 4, 5, 7, 10, 11 }; //modify this array appropriately by reassignment
+
+	printf("%d\n", evens[2]);
+
+	odds[1] = 3;	//reassigned second and fifth elements
+	odds[4] = 9;
 
 	return 0;
 }
