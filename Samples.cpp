36e7b77c0ca0a086fcaab623e5cc3b02b869b132
nwong02 <nelsonwong107@gmail.com>
2022-02-21 20:25:41 -0500

    Looping arrays adde d

--- Samples.cpp
+++ Samples.cpp
@@ -23,5 +23,22 @@ int main(void)
 	odds[1] = 3;	//reassigned second and fifth elements
 	odds[4] = 9;
 
+
+	/* Looping through arrays */
+	int arr[] = {3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 20, 22, 26, 28, 29};
+	int arr2[100];
+
+	for (int i = 0; i < 20; i++)
+	{
+		printf("%d\n", i);
+	}
+
+	for (int i = 0; i < 100; i++)
+	{
+		arr2[i] = i + 4;
+		printf("%d\n", i);
+	}
+
+
 	return 0;
 }
